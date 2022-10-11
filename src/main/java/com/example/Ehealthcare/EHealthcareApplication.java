f9736ebc1c7e631774299c2c8ccac8497c80c822
Rupesh Mohanty <rupeshmohanty67@gmail.com>
2022-10-11 20:48:29 +0530

    Initial code added

---
+++ src/main/java/com/example/Ehealthcare/EHealthcareApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ehealthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHealthcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EHealthcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ehealthcare/EHealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ehealthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHealthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
